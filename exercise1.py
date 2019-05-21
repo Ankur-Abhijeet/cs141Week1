c824145e1d784b883574c2c326ddd9670750e418
subha-mishra <50800258+subha-mishra@users.noreply.github.com>
2019-05-22 00:18:50 +0530

    Create exercise1.py

---
+++ exercise1.py
@@ -0,0 +1,8 @@
+# Week 1 : Exercise 1
+# Complete the following exercises
+
+# 1. Store a message in a variable, and then print that message.
+
+# 2. Store a message in a variable, and print that message . Then change the value of your variable to a new message, and print the new message .
+
+
