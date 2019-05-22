01bbed6cc38ff8a57d4e7711b5fe2ba1728f48dd
subha-mishra <50800258+subha-mishra@users.noreply.github.com>
2019-05-22 15:30:07 +0530

    Updated exercise1.py

--- exercise1.py
+++ exercise1.py
@@ -1,8 +1,21 @@
-# Week 1 : Exercise 1
-# Complete the following exercises
+'''
+Week 1 : Setting up python environment. 
 
-# 1. Store a message in a variable, and then print that message.
+1. Download python: 
+https://www.python.org/downloads/
 
-# 2. Store a message in a variable, and print that message . Then change the value of your variable to a new message, and print the new message .
+2. Download Visual Studio Code: 
+https://code.visualstudio.com
 
+3. Add the Python extension of Visual Studio Code: 
 
+4. Create an account on Github
+https://github.com
+
+5. Download this assignment and mark as done on all points. 
+
+6. Sample program to print("hello") 
+print("hello")  
+'''
+
+#Complete the program to print hello and submit. 
